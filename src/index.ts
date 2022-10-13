319ab8fe0992f1f5974ff8ec431b39b7cacd4721
MohammedAlmanea <xrkodx1@gmail.com>
2022-10-13 15:03:12 +0300

    Set up endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('hello');
+import express from 'express';
+import routes from './routes/.';
+
+const app = express();
+const port = 3000;
+
+app.use('/main', routes);
+
+app.listen(port, () => {
+  console.log(`server started at ${port}`);
+});
+
+export default app;
