275bad9bea17e6919197886d9c9b9122f658116c
MohammedAlmanea <xrkodx1@gmail.com>
2022-10-14 16:21:20 +0300

    Imported sharp need to complete function

--- src/routes/api/process.ts
+++ src/routes/api/process.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import sharp from 'sharp';
 
 const process = express.Router();
 
-process.get('/', async (req, res) => {
+process.get('/', async (req:express.Request, res:express.Response) => {
   res.send('connected to process');
+  let width:number = req.query.width as unknown as number;
+  let height:number = req.query.height as unknown as number;
+  let imageName:string = req.query.imageName as string;
+ 
 });
 
 export default process;
