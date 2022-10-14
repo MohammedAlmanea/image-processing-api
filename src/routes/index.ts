4b5f326a6a2b27c50dfcc2d8c743f4b165e5d367
MohammedAlmanea <xrkodx1@gmail.com>
2022-10-14 16:02:37 +0300

    Connected process to routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,12 @@
-import express from 'express';
+import express, { Router } from 'express';
+import process from './process/process';
 
 const routes = express.Router();
 
 routes.get('/', (req, res) => {
   res.send('Welcome to the main page');
 });
+
+routes.use('/process', process);
 
 export default routes;
