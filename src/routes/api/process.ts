633993c600a9284fef45e06801814b458604cbe8
MohammedAlmanea <xrkodx1@gmail.com>
2022-10-14 19:34:55 +0300

    Created working processImage that sends the resized picture

--- src/routes/api/process.ts
+++ src/routes/api/process.ts
@@ -1,14 +1,30 @@
 import express from 'express';
 import sharp from 'sharp';
+import fs from 'fs';
+import path from 'path';
 
 const process = express.Router();
 
 process.get('/', async (req:express.Request, res:express.Response) => {
-  res.send('connected to process');
-  let width:number = req.query.width as unknown as number;
-  let height:number = req.query.height as unknown as number;
-  let imageName:string = req.query.imageName as string;
- 
+  const width:number = parseInt(req.query.width as string);
+  const height:number = parseInt(req.query.height as string);
+  const imageName:string = req.query.imageName as string;
+  if(fs.existsSync(`./images/cache/${imageName}-${width}-${height}.png`))
+    {
+      res.sendFile(`${path.resolve('./')}/images/cache/${imageName}-${width}-${height}.png`)
+    }
+    else{
+  const processImage = await sharp(`./images/${imageName}.jpg`)
+  .resize(width,height)
+    .png()
+    .toBuffer()
+
+    res.sendFile(`${path.resolve('./')}/images/cache/${imageName}-${width}-${height}.png`)
+      fs.writeFile(`./images/cache/${imageName}-${width}-${height}.png`,processImage,(err) =>{
+      if(err)
+      console.log(`saving file failed: ${err}`);
+    })
+  }
 });
 
 export default process;
