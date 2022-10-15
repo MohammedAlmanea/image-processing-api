ef26c83f0466ebe10f8c70fe38a13147fccf0816
MohammedAlmanea <xrkodx1@gmail.com>
2022-10-15 12:20:19 +0300

    Fixed bug and formatted the code

--- src/routes/api/process.ts
+++ src/routes/api/process.ts
@@ -5,25 +5,34 @@ import path from 'path';
 
 const process = express.Router();
 
-process.get('/', async (req:express.Request, res:express.Response) => {
-  const width:number = parseInt(req.query.width as string);
-  const height:number = parseInt(req.query.height as string);
-  const imageName:string = req.query.imageName as string;
-  if(fs.existsSync(`./images/cache/${imageName}-${width}-${height}.png`))
-    {
-      res.sendFile(`${path.resolve('./')}/images/cache/${imageName}-${width}-${height}.png`)
-    }
-    else{
-  const processImage = await sharp(`./images/${imageName}.jpg`)
-  .resize(width,height)
-    .png()
-    .toBuffer()
+process.get('/', async (req: express.Request, res: express.Response) => {
+  const width: number = parseInt(req.query.width as string);
+  const height: number = parseInt(req.query.height as string);
+  const imageName: string = req.query.imageName as string;
+  if (fs.existsSync(`./images/cache/${imageName}-${width}-${height}.png`)) {
+    res.sendFile(
+      `${path.resolve('./')}/images/cache/${imageName}-${width}-${height}.png`
+    );
+  } else {
+    const processImage = await sharp(`./images/${imageName}.jpg`)
+      .resize(width, height)
+      .png()
+      .toBuffer();
 
-    res.sendFile(`${path.resolve('./')}/images/cache/${imageName}-${width}-${height}.png`)
-      fs.writeFile(`./images/cache/${imageName}-${width}-${height}.png`,processImage,(err) =>{
-      if(err)
-      console.log(`saving file failed: ${err}`);
-    })
+    fs.writeFile(
+      `./images/cache/${imageName}-${width}-${height}.png`,
+      processImage,
+      (err) => {
+        if (err) {
+          console.log(`saving file failed: ${err}`);
+        }
+        res.sendFile(
+          `${path.resolve(
+            './'
+          )}/images/cache/${imageName}-${width}-${height}.png`
+        );
+      }
+    );
   }
 });
 
