bf732c2617f3fc5c13fa8583ebc5e65560d1fdaa
MohammedAlmanea <xrkodx1@gmail.com>
2022-10-16 14:10:38 +0300

    Factored processImage function to its own file

---
+++ src/utilities.ts
@@ -0,0 +1,17 @@
+import sharp from 'sharp';
+import fs from 'fs';
+const processImage = async (width:number,height:number,imageName:string) =>{
+
+const img = await sharp(`./images/${imageName}.jpg`)
+      .resize(width, height)
+      .png()
+      .toBuffer();
+
+    // Saving new image in cache
+      fs.writeFileSync(
+      `./images/cache/${imageName}-${width}-${height}.png`,
+      img,
+    );
+}
+
+export default processImage;
